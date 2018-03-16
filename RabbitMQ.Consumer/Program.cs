5955a7d67f40148d32c7814a08ff7097de5d9ce8
joaovictorino <jhvictorino@gmail.com>
2018-03-16 15:13:25 +0100

    versão inicial

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,139 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExemploAprovaRapido();
+
+            //ExemploFilaSimples(args);
+
+            //ExemploVariosConsumidores(args);
+
+            ExemploPubSub(args);
+        }
+
+        private static void ExemploPubSub(string[] args)
+        {
+        }
+
+        private static void ExemploVariosConsumidores(string[] args)
+        {
+            //criar a conexão
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //declarar a fila
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //permite que apenas uma msg seja processada por vez (prefetch)
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume(queue: "task_queue",
+                                     autoAck: false,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static void ExemploFilaSimples(string[] args)
+        {
+            //conexão
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //declaração da fila
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //evento para cada mensagem recebida na fila
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                };
+
+                //assinatura da fila
+                channel.BasicConsume(queue: "hello",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static void ExemploAprovaRapido()
+        {
+            //conexão
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //criar exchange (broker)
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                //obter fila do exchange
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "logs",
+                                  routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                //evento disparado para cada mensagem
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+
+                //assinar fila
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Sender/Program.cs
@@ -0,0 +1,159 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Framing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ExemploAprovaRapido(args);
+
+            //ExemploFilaSimples(args);
+
+            //ExemploVariosConsumidores(args);
+
+            ExemploPubSub(args);
+        }
+
+        private static void ExemploPubSub(string[] args)
+        {
+        }
+
+        private static void ExemploVariosConsumidores(string[] args)
+        {
+            //cria conexão
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //declara afila como durável
+                channel.QueueDeclare(queue: "task_queue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                //criar a mensagem
+                var message = GetMessageBroadcast(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //declara a mensagem como persistente
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                //publica a mensagem na fila
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessageBroadcast(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+
+        private static void ExemploFilaSimples(string[] args)
+        {
+            //conexão
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //declaração fila como não durável
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                //mensagem
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //publicação na fila
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static void ExemploAprovaRapido(string[] args)
+        {
+            //conexão
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //parametros da fila
+                Dictionary<string, object> valores = new Dictionary<string, object>();
+                //redirecionamento no timeout
+                valores.Add("x-dead-letter-exchange", "logs");
+                //tempo de timeout
+                valores.Add("x-message-ttl", 5000);
+                //declaração da fila
+                channel.QueueDeclare(queue: "logs_dlx",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: valores);
+
+                //texto da mensagem
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //publicação da mensagem na fila
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "logs_dlx",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+
+            //conexão
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //declaração do exchange
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                //montar a mensagem
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //publicação na fila
+                channel.BasicPublish(exchange: "logs",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0)
+                   ? string.Join(" ", args)
+                   : "info: Hello World!");
+        }
+    }
+}
